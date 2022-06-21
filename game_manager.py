23aeb499384c5c1fa795d2d21ab4cd22c19b6f54
Arthur368 <77182950+Arthur368@users.noreply.github.com>
2022-06-21 23:02:09 +1000

    update

--- game_manager.py
+++ game_manager.py
@@ -1,3 +1,4 @@
+from more_itertools import sample
 import pygame as pg
 from pygame.locals import *
 from game_object import *
@@ -9,7 +10,7 @@ class Game_manager:
     def __init__(self) -> None:
         self.screen = pg.display.set_mode((800, 600))
         self.running = True
-        self.current_board = Board(40, 200, "images/board.jpg")
+        self.current_board = Board(20, 50, "images/board.jpg", 535, 535)
     
     def tick(self) -> None:
 
@@ -18,7 +19,12 @@ class Game_manager:
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
+
+        sample_piece = Piece(43 - 122/7, 73, "images/black_piece.png", 244/7, 244/7)
+        sample_piece_2 = Piece(43 + 122/7, 73, "images/black_piece.png", 244/7, 244/7)
         
         self.current_board.draw(self.screen)
+        sample_piece.draw(self.screen)
+        sample_piece_2.draw(self.screen)
 
         pg.display.update()
--- game_object.py
+++ game_object.py
@@ -3,23 +3,28 @@ from pygame.locals import *
 
 class Game_object:
 
-    def __init__(self, x : int, y : int, image_path: str) -> None:
+    def __init__(self, x : int, y : int, image_path: str, length: int, width: int) -> None:
 
         self.x = x
         self.y = y
         self.image_path = image_path
+        self.length = length
+        self.width = width
         self.image = pg.image.load(image_path)
+        self.image = pg.transform.scale(self.image, (self.length, self.width))
         
     def draw(self, screen: pg.surface) -> None:
-        
+
         screen.blit(self.image, (self.x, self.y))
 
 class Board(Game_object):
 
-    def __init__(self, x: int, y: int, image_path: str) -> None:
-        super().__init__(x, y, image_path)
+    def __init__(self, x: int, y: int, image_path: str, length: int, width: int) -> None:
+        super().__init__(x, y, image_path, length, width)
+    
+        
 
 class Piece(Game_object):
 
-    def __init__(self, x: int, y: int, image_path: str) -> None:
-        super().__init__(x, y, image_path)
+    def __init__(self, x: int, y: int, image_path: str, length: int, width: int) -> None:
+        super().__init__(x, y, image_path, length, width)
