4644f6cdcdb4dd1839bdc379e7357ff0ce8f547d
Arthur368 <77182950+Arthur368@users.noreply.github.com>
2022-06-24 00:03:35 +1000

    Add some functions
    Implement some basic functions of the game, including place pieces by mouse clicking and check if there is five in a row(diagonal need to be implemented next)

--- game_manager.py
+++ game_manager.py
@@ -11,7 +11,28 @@ class Game_manager:
         self.screen = pg.display.set_mode((800, 600))
         self.running = True
         self.current_board = Board(20, 50, "images/board.jpg", 535, 535)
+        self.turn = "black"
+        self.page = 1 # page 1 for game page
+
+    def change_turn(self):
+
+        if self.turn == "black":
+            self.turn = "white"
+        else:
+            self.turn = "black"
     
+    def game_page(self) -> None:
+
+        # show the board
+        self.current_board.draw(self.screen)
+
+        # show all pieces
+        for row in self.current_board.board_for_display:
+            for piece in row:
+                if piece != None:
+                    piece.draw(self.screen)
+
+
     def tick(self) -> None:
 
         for event in pg.event.get():
@@ -20,11 +41,24 @@ class Game_manager:
                 pg.quit()
                 exit()
 
-        sample_piece = Piece(43 - 122/7, 73, "images/black_piece.png", 244/7, 244/7)
-        sample_piece_2 = Piece(43 + 122/7, 73, "images/black_piece.png", 244/7, 244/7)
-        
-        self.current_board.draw(self.screen)
-        sample_piece.draw(self.screen)
-        sample_piece_2.draw(self.screen)
+            if self.page == 1:
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+
+                        pos = event.pos # position of press
+
+                        row = int((pos[0] - 43 + 122/7) // (244/7))
+                        col = int((pos[1] - 73 + 122/7) // (244/7))
+
+                        if 0 <= row < 15 and 0 <= col < 15: # check if click inside the board
+                            self.current_board.add_piece(row, col, self.turn) # turn is the same as color and type of a piece
+
+                            if not self.current_board.is_terminal():
+                                self.change_turn()
+                            else:
+                                pg.quit()
+                                exit()
+
+        self.game_page()
 
         pg.display.update()
--- game_object.py
+++ game_object.py
@@ -1,5 +1,11 @@
+import imp
+from multiprocessing import Condition
+from typing import Optional
+from xmlrpc.client import Boolean
 import pygame as pg
 from pygame.locals import *
+import numpy as np
+import re
 
 class Game_object:
 
@@ -21,10 +27,50 @@ class Board(Game_object):
 
     def __init__(self, x: int, y: int, image_path: str, length: int, width: int) -> None:
         super().__init__(x, y, image_path, length, width)
-    
+        self.board_for_display = np.empty((15, 15), dtype=Piece)
+        self.state = np.empty((15, 15), dtype=str)
+        self.state[:] = "n"
+
+    # add a piece with a color(black or white into a board), only when there is a position
+    def add_piece(self, row: int, col: int, color: str) -> None:
         
+        # color for the type of piece
+
+        if self.board_for_display[row][col] == None:
+
+            self.board_for_display[row][col] = Piece(43 + (row - 1/2)*(244/7), 73 + (col - 1/2)*(244/7), "Images/{}_piece.png".format(color), 244/7, 244/7, color)
+            self.state[row][col] = color[0]
+
+    def is_five_in_a_row(self, rows: list) -> Boolean:
+
+        num_of_five_in_a_row = sum([len(re.findall("b"*5, row)) for row in rows]) + sum([len(re.findall("w"*5, row)) for row in rows])
+
+        if num_of_five_in_a_row > 0:
+            return True
+        else:
+            return False
+
+    def is_terminal(self) -> Boolean:
+
+        rows = ["".join(row) for row in self.state] # get rows
+
+        state_T = self.state.transpose()
+        cols = ["".join(col) for col in state_T] # get cols
+
+        """
+        seems there is a little bit weird about which one is row and which one is col
+        """
+
+        #print(rows)
+
+        is_terminal = self.is_five_in_a_row(rows) or self.is_five_in_a_row(cols)
+
+        return is_terminal
+
+
 
 class Piece(Game_object):
 
-    def __init__(self, x: int, y: int, image_path: str, length: int, width: int) -> None:
+    def __init__(self, x: int, y: int, image_path: str, length: int, width: int, piece_type: str) -> None:
         super().__init__(x, y, image_path, length, width)
+        self.piece_type = piece_type
