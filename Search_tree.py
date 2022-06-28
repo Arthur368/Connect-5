0c213a6b8c86f84b32b3508c972a344e358013f7
Arthur <chenqikun2001@163.com>
2022-06-29 01:12:51 +1000

    Min-Max

--- Search_tree.py
+++ Search_tree.py
@@ -11,14 +11,9 @@ class Node:
         self.parent = None
         self.children = []
         
-        self.cached_columns = []
-        self.cached_diags = []
-
-        self.is_terminal = False
+        #self.is_terminal = False
         self.is_evaluated = False
-
-        #self.lowerbound = 100000
-        #self.upperbound = -100000
+              
         self.eval_for_prop = 0
 
         if self.player == "black":
@@ -26,240 +21,191 @@ class Node:
         else:
             self.eval_for_prop = 100000
 
+        self.bound = 0
+
         self.opponent = ""
         
         if self.player == "black":
             self.opponent = "white"
         else:
-            self.opponect = "black"
+            self.opponent = "black"
 
-    def add_child(self, node):
-        self.children.append(node)
-    
-    def set_parent(self, node):
-        self.parent = node
-
-    def evaluation(self):
-        return self.score("black") - self.score("white")
-    
-
-    def score(self, player):
-        value = 10 * self.num_in_a_row(2, player) + 100 * self.num_in_a_row(3, player) + 1000 * self.num_in_a_row(4, player)
-        
-        return value
-  
-    def check_terminal(self):
-        if self.num_in_a_row(4, "red") > 0:
-            self.is_terminal = True
-            self.eval += 10000
-            self.eval_for_prop = 10000
-        
-        if self.num_in_a_row(4, "yellow") > 0:
-            self.is_terminal = True
-            self.eval -= 10000
-            self.eval_for_prop = -10000
-    
-    def num_in_a_row(self, rows: list, num: int, player: str) -> int:
-
-        num_in_a_row = sum([len(re.findall(player[0]*num, row)) for row in rows]) 
-        
-        return num_in_a_row
-
-    def Check_terminal(self):
-
-        rows = ["".join(row) for row in self.state] # get rows
+        self.rows = ["".join(row) for row in self.state] # get rows
 
         state_T = self.state.transpose()
-        cols = ["".join(col) for col in state_T] # get cols
+        self.cols = ["".join(col) for col in state_T] # get cols
 
         """
         seems there is a little bit weird about which one is row and which one is col
         """
         #print(rows)
 
-        diags = ["".join(self.state.diagonal(i)) for i in range(-14, 15)]
+        self.diags = ["".join(self.state.diagonal(i)) for i in range(-14, 15)]
 
         state_filped = np.fliplr(self.state)
 
-        diags.extend(["".join(state_filped.diagonal(i)) for i in range(-14, 15)])
+        self.diags.extend(["".join(state_filped.diagonal(i)) for i in range(-14, 15)])
+    
+        self.is_terminal = self.Check_terminal()
 
-        is_terminal = self.num_in_a_row(rows, 5, "black") + self.num_in_a_row(cols, 5, "black") + self.num_in_a_row(diags, 5, "black") + self.num_in_a_row(rows, 5, "white") + self.num_in_a_row(cols, 5, "white") + self.num_in_a_row(diags, 5, "white")
+        self.newest_pos = (-1, -1) # denote the newest move
+    
+    def add_child(self, node) -> None:
+        self.children.append(node)
+    
+    def set_parent(self, node) -> None:
+        self.parent = node
+
+    def set_newest_pos(self, pos: tuple) -> None:
+        self.newest_pos = pos
+
+    def evaluation(self) -> None:
+
+        self.eval = self.score("black") - self.score("white")
+    
+
+    def score(self, player: str) -> int:
+
+        p = player[0]
+
+        if player == "black":
+            o = "w" # o for opponent
+        else:
+            o = "b" 
+
+        value = 10000* self.detect_pattern(p*5) + 8000*self.detect_pattern("n{}n".format(p*4))
+        value += 4000* (self.detect_pattern("{}{}n".format(o, p*4)) + self.detect_pattern("n{}{}".format(p*4, o)))  
+        value += 4000* sum([self.detect_pattern("{}n{}".format(p*i, p*(4 - i))) for i in range(1, 4)])
+        value += 4000* (self.detect_pattern("n{}$".format(p*4)) + self.detect_pattern("^{}n".format(p*4)))
+        value += 2500* self.detect_pattern("n{}n".format(p*3))
+        value += 1000* sum([self.detect_pattern("n{}n{}n".format(p*i, p*(3 - i))) for i in range(1, 3)])
+        value += 100 * (self.detect_pattern("nn{}n".format(p*2)) + self.detect_pattern("n{}nn".format(p*2)))        
+        value += sum([self.detect_pattern("{}{}{}".format("n"*i, p, "n"*(4 - i))) for i in range(1, 4)])
         
-        return is_terminal > 0
+        return value
+   
+    # find patterns for the whole board by reuse
+    def detect_pattern(self, pattern: str) -> int:
+
+        return self.num_in_a_row(self.rows, pattern) + self.num_in_a_row(self.cols, pattern) + self.num_in_a_row(self.diags, pattern)
+
+    # find patterns for rows
+    def num_in_a_row(self, rows: list, pattern: str) -> int:
+
+        num_in_a_row = sum([len(re.findall(pattern, row)) for row in rows]) 
+        
+        return num_in_a_row
+
+    def Check_terminal(self):
+
+        five_in_a_row = self.detect_pattern("b"*5) + self.detect_pattern("w"*5)
+        
+        return five_in_a_row > 0
 
 class Game_Tree:
 
-    def __init__(self, root):
+    def __init__(self, root) -> None:
         self.root = root
         self.evaluated_node_size = 0
 
-    def generate_game_tree(self, player, current_depth, max_depth):
+    def generate_game_tree(self, current_depth, max_depth):
 
         if current_depth < max_depth and not self.root.is_terminal:
-        
-            rows = self.root.state.split(",")
-            cpy_rows = deepcopy(rows)
-            
-            num_of_rows = len(rows)
-            num_of_columns = len(rows[0])
-            p = player[0]
 
-            
-            new_player = ''
-            if player == "red":
-                new_player = "yellow"
-            else:
-                new_player = "red"
-        
-            
-            #opt_state = ''
-            first_node = True
+            state = deepcopy(self.root.state)
 
-            if self.root.parent != None:
-                for child in self.root.parent.children:
+            # loop over and find all possible places for pieces.
+            for i in range(len(state)):
 
-                    if child.state == "illegal":
-                        pass
-                    else:
-                        if child.state == self.root.state:
-                            first_node = True
-                        else:
-                            first_node = False
+                for j in range(len(state[0])):
+
+                    if state[i][j] == "n":
                         
-                        break
+                        #print(self.root.opponent)
+                        state[i][j] = self.root.player[0]
+                        child = Node(state, self.root.opponent)
+                        #print(child.opponent == "")
+                        child.set_newest_pos((i, j)) # for return best move
 
-            for i in range(num_of_columns):
-                
-                opt_state = ''
+                        self.root.add_child(child)
+                        child.set_parent(self.root)
 
-                # find a place for legal move
-                for j in range(num_of_rows):
-                    score = 0
-                    new_state = ''
-                    if cpy_rows[j][i] == ".":
-                        cpy_rows[j] = cpy_rows[j][0:i] + p + cpy_rows[j][i+1:]
-                        new_state = ",".join(cpy_rows)
+                        subtree = Game_Tree(child)
+                        subtree.generate_game_tree(current_depth + 1, max_depth)
 
-                        #print(new_state)
-                        
-                        temp_node = Node(new_state, new_player)
-                        score = temp_node.evaluation()
+                        state[i][j] = "n" # need to change board back before next iteration.
 
 
-                        opt_state = new_state
-                        cpy_rows = deepcopy(rows)
+    def evaluate_game_tree(self) -> int:
 
-                        break
-
-                if opt_state != '':
-
-                    new_game_node = Node(opt_state, new_player)
-                    self.root.add_child(new_game_node)
-                    new_game_node.set_parent(self.root)                    
-                    #print(new_game_node.state)
-
-                    new_game_node.check_terminal()
-                    subtree = Game_Tree(new_game_node)
-
-                    #if not new_game_node.is_terminal:
-                    subtree.generate_game_tree(new_player, current_depth + 1, max_depth)
-
-                    if first_node and current_depth == max_depth - 1:
-                        pass                      
-                    else:
-                        
-                        if self.root.player == "red":
-                            
-                            if self.root.parent != None:
-                                if self.root.eval_for_prop >= self.root.parent.eval_for_prop:
-                                    break
-                            
-                                #if self.root.parent.parent != None:
-                                #    if self.root.eval_for_prop <= self.root.parent.parent.eval_for_prop:
-                                #        break
-                        else:
-                            
-                            if self.root.parent != None:
-                                if self.root.eval_for_prop <= self.root.parent.eval_for_prop:
-                                    break
-
-                                #if self.root.parent.parent != None:
-                                #    if self.root.eval_for_prop >= self.root.parent.parent.eval_for_prop:
-                                #        break          
-                               
-                        if max_depth == 4 and self.root.count_token("red") + self.root.count_token("yellow") == 1:
-                            if self.root.eval_for_prop == -10:
-                                break
-                else:
-                    new_game_node = Node("illegal", new_player)
-                    self.root.add_child(new_game_node)
-                    new_game_node.set_parent(self.root)
-            """
-            if self.root.state != "illegal":
-                self.root.is_evaluated = True
-
-                if self.root.parent != None:
-
-                    if self.root.player == "red":
-                        if self.root.eval_for_prop < self.root.parent.eval_for_prop:
-                            self.root.parent.eval_for_prop = self.root.eval_for_prop
-                    else:
-                        if self.root.eval_for_prop > self.root.parent.eval_for_prop:
-                            self.root.parent.eval_for_prop = self.root.eval_for_prop   
-            """
-            if self.root.state != "illegal":
-                self.root.is_evaluated = True
-                        
-        
-        
-        elif current_depth == max_depth or self.root.is_terminal:
+        if len(self.root.children) == 0: # leaf node, need to be evaluated
             
-            if self.root.eval == 0 and self.root.state != "illegal":
-                self.root.eval = self.root.evaluation()
-                self.root.eval_for_prop = self.root.eval
-                #print(self.root.evaluation())
-                self.root.is_evaluated = True
-                #print("T")
-            elif self.root.eval == 10000 or self.root.eval == -10000:
-                self.root.eval_for_prop = self.root.eval
-                self.root.is_evaluated = True
+            self.root.evaluation()
 
+        else:
             
-        if self.root.state != "illegal" and self.root.parent != None:
+            evals = []
 
-            if self.root.player == "red":
-                if self.root.eval_for_prop < self.root.parent.eval_for_prop:
-                    self.root.parent.eval_for_prop = self.root.eval_for_prop
+            for child in self.root.children:
+
+                if self.root.children.index(child) == 0: # we have to evaluate every thing for first child so we get something to compare.
+
+                    if len(child.children) == 0:
+                        
+                        evals.append(Game_Tree(child).evaluate_game_tree())
                     
-                                   
-            else:
-                if self.root.eval_for_prop > self.root.parent.eval_for_prop:
-                    self.root.parent.eval_for_prop = self.root.eval_for_prop
-
-            """
-            if self.root.parent.parent != None:
+                    else:
                 
-                if self.root.player == "red":
-                    if self.root.eval_for_prop > self.root.parent.parent.eval_for_prop:
-                        self.root.parent.parent.eval_for_prop = self.root.eval_for_prop
+                        evals_for_child = [Game_Tree(grandchild).evaluate_game_tree() for grandchild in child.children]
 
-                    
+                        if child.player == "black":
+                            
+                            evals.append(max(evals_for_child))
+
+                        else:   
+                            
+                            evals.append(min(evals_for_child))
+
+                     # keep bounds not exact values for the sake of pruning.
+
+                    self.root.bound = evals[0]
+                
                 else:
-                    if self.root.eval_for_prop < self.root.parent.parent.eval_for_prop:
-                        self.root.parent.parent.eval_for_prop = self.root.eval_for_prop
-            """
-    
-            #print("T")
-        
-            
+                    # condition for pruning
+                    if self.root.parent != None and ((self.root.player == "black" and self.root.parent.bound <= self.root.bound) or (self.root.player == "white" and self.root.parent.bound >= self.root.bound)):
+                        break
+                    
+                    child_value = Game_Tree(child).evaluate_game_tree()
+
+                    evals.append(Game_Tree(child).evaluate_game_tree())
+                    
+            if self.root.player == "black":
+                self.root.eval = max(evals)
+            else:
+                self.root.eval = min(evals)
+
+        return self.root.eval
 
     def choose(self):
+        
+        best_move = (-1, -1)
+        best_value = 0.5
+
         for child in self.root.children:
-            #print(child.eval)
-            if child.state != "illegal":
-                #print(child.eval_for_prop)
-                if child.eval_for_prop == self.root.eval_for_prop:
-                    return self.root.children.index(child)
+            
+            if best_value == 0.5:
+               
+                best_value = child.eval
+                best_move = child.newest_pos
+            else:
+                
+                if (self.root.player == "black" and child.eval > best_value) or (self.root.player == "white" and child.eval < best_value):
+
+                    best_value = child.eval
+                    best_move = child.newest_pos
+
+        return best_move
+
     
     def count_evaluated_nodes(self):
 
@@ -287,27 +233,31 @@ class Game_Tree:
 
   
 def connect_four_ab(contents, turn, max_depth):
-    #game_tree = Game_Tree(Node(contents, turn))
+    game_tree = Game_Tree(Node(contents, turn))
 
-    #game_tree.generate_game_tree(turn, 0, int(max_depth))
+    game_tree.generate_game_tree(0, int(max_depth))
 
-    #action = game_tree.choose()
+    game_tree.evaluate_game_tree()
+
+    action = game_tree.choose()
     
     #game_tree.evaluated_node_size += game_tree.count_evaluated_nodes()
     #return str(action) + "\n" + str(game_tree.evaluated_node_size)
     
     node = Node(contents, turn)
 
-    return node.evaluation()
+    #print(node.rows)
+
+    return action
 
 if __name__ == '__main__':
     # Example function call below, you can add your own to test the connect_four_mm function
     board = np.empty((15, 15), dtype = str)
     board[:] = "n"
-    board[0][1:6] = "b"
-    board[3][3:5] = "w"
+    #board[0][1:6] = "b"
+    #board[3][3:5] = "w"
     
-    print(connect_four_ab(board, "black", 5))
+    print(connect_four_ab(board, "black", 2))
     
 
     
